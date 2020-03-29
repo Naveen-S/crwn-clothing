8ed65f753afe130b8c5ed370779bc26032f3de18
naveen <naveentejas1109@gmail.com>
2020-03-29 14:22:03 +0530

    React-router-dom and withRouter HOC.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {withRouter} from 'react-router-dom';
 import './menu-item.style.scss';
 
-export default function MenuItem({title, imageUrl, size}) {
+function MenuItem({title, imageUrl, size, linkUrl, history, match}) {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={
+            () => { 
+                console.log(match);
+                history.push(`${match.url}${linkUrl}`);
+                }
+            }>
             <div className="background-img" style={{backgroundImage: `url(${imageUrl})`}}>
             </div>
             <div className="content">
@@ -13,3 +19,6 @@ export default function MenuItem({title, imageUrl, size}) {
         </div>
     )
 }
+
+// withRouter is an HOC which gives access to history, match and location props from BrowserRouter.
+export default withRouter(MenuItem);
