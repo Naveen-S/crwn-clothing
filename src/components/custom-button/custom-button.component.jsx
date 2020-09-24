37b08aa39359c9daa993d31089c2b24d29e97d37
Naveen S <naveentejas1109@gmail.com>
2020-09-24 10:18:16 +0530

    Added styled component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,15 +1,85 @@
-import React from 'react'
-import './custom-button.style.scss';
+import React from "react";
+import styled, { css } from "styled-components";
+import "./custom-button.style.scss";
 
-export default function CustomButton({ children, inverted, isGoogleSignIn, ...OtherProps }) {
-    return (
-        <button className=
-        {
-            `${ inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-signin' : ''} custom-button`
-        } 
-            {...OtherProps} > 
-            
-            {children} 
-            </button>
-    )
+export default function CustomButton({
+  children,
+  inverted,
+  isGoogleSignIn,
+  ...OtherProps
+}) {
+  return (
+    // <button
+    //   className={`${inverted ? "inverted" : ""} ${
+    //     isGoogleSignIn ? "google-signin" : ""
+    //   } custom-button`}
+    //   {...OtherProps}
+    // >
+    //   {children}
+    // </button>
+
+    <Button isGoogleSignIn={isGoogleSignIn} inverted={inverted} {...OtherProps}>
+        {children}
+    </Button>
+  );
 }
+
+const getButtonStyle = (props) => {
+    if(props.isGoogleSignIn) {
+        return googleSignInStyle;
+    }
+
+    return props.inverted? invertedStyle:basicStyle;
+}
+
+const basicStyle = css`
+  background-color: black;
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
+const invertedStyle = css`
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+
+    &:hover {
+        background-color: black;
+        color: white;
+        border: none;
+    }
+`;
+
+const googleSignInStyle = css`
+    background-color: #4285f4;
+    color: white;
+
+    &:hover {
+        background-color: #357ae8;
+        border: none;
+    }
+    &:focus {
+        outline: none;
+    }
+`;
+
+const Button = styled.button`
+  min-width: 165px;
+  width: 100%;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  border: none;
+  cursor: pointer;
+  ${getButtonStyle}
+`;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,32 +1,58 @@
 import React from 'react'
 import {connect} from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crwn.svg';
 import {auth} from '../../firebase/firebase.util';
 import CartIcon from '../cart-icon/cartIcon';
 import CartDropDown from '../cart-dropdown/cartDropdown';
 
-import './header.style.scss';
+import { HeaderContainer, LogoContainter, OptionContainer, OptionDiv, OptionLink} from './header.styled';
 
-function Header({currentUser, isCartOpen}) {
+// import './header.style.scss';
+
+// Normal
+// function Header({currentUser, isCartOpen}) {
     
+//     return (
+//         <div className="header">
+//             <Link className="logo-container" to="/">
+//                 <Logo className="logo" to="/"> </Logo>
+//             </Link>
+//             <div className="options">
+//                 <Link to="/shop" className="option"> SHOP</Link>
+//                 <Link to="/contact" className="option"> CONTACT</Link>
+//                 {
+//                     currentUser?
+//                     <div className="option" onClick={() => auth.signOut()}> SIGN OUT</div>:
+//                     <Link to="/signin" className="option">SIGN IN</Link>
+//                 }
+//                 <CartIcon />
+//             </div>
+//             { isCartOpen && <CartDropDown />}
+//         </div>
+//     )
+// }
+
+
+// With styled component
+function Header({currentUser, isCartOpen}) {
     return (
-        <div className="header">
-            <Link className="logo-container" to="/">
-                <Logo className="logo" to="/"> </Logo>
-            </Link>
-            <div className="options">
-                <Link to="/shop" className="option"> SHOP</Link>
-                <Link to="/contact" className="option"> CONTACT</Link>
+        <HeaderContainer>
+            <LogoContainter to="/">
+                <Logo to="/"></Logo>
+            </LogoContainter>
+            <OptionContainer>
+                <OptionLink to="/shop"> SHOP</OptionLink>
+                <OptionLink to="/contact">CONTACT</OptionLink>
                 {
                     currentUser?
-                    <div className="option" onClick={() => auth.signOut()}> SIGN OUT</div>:
-                    <Link to="/signin" className="option">SIGN IN</Link>
+                    <OptionDiv onClick={() => auth.signOut()}> SIGN OUT </OptionDiv> :
+                    <OptionLink to="/signin"> SIGN IN</OptionLink>
                 }
                 <CartIcon />
-            </div>
+            </OptionContainer>
             { isCartOpen && <CartDropDown />}
-        </div>
+        </HeaderContainer>
     )
 }
 
